ab01f0c5ea797e2adce4f051b919e98134102f79
duongbui <duongbuibd@gmail.com>
2025-01-03 16:41:13 +0700

    implement linked list with append and traversal methods

---
+++ linked_list/function.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+        current_node = self.head
+        while current_node.next:
+            current_node = current_node.next
+        current_node.next = new_node
+    
+    def traverseAndPrint(self):
+        currentNode = self.head
+        while currentNode:
+            print(currentNode.data, end=" -> ")
+            currentNode = currentNode.next
+        print("null")
+
---
+++ linked_list/main.py
@@ -0,0 +1,8 @@
+from function import *
+
+# Usage
+ll = LinkedList()
+ll.append(1)
+ll.append(2)
+ll.append(3)
+ll.traverseAndPrint()  # Outputs: 1 -> 2 -> 3 -> null
